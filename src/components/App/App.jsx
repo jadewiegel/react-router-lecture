49cc4409db48a47fd2528ca439fbd08f7495315e
Jade Wiegel <jadewiegel@gmail.com>
2023-01-17 20:24:06 -0600

    pushing to github to save for next lecture

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,51 @@
 import React from 'react';
+import { HashRouter as Router, Route, Link, useHistory } from 'react-router-dom';
 import './App.css';
 
 import Animals from '../Animals/Animals.jsx';
 import Plants from '../Plants/Plants.jsx';
 import Home from '../Home/Home.jsx';
 
+
 function App() {
+  const history = useHistory();
+  const routeHome = () => {
+    alert('There is no place like home');
+    history.push('/');
+  }
+
   return (
-    <div className="App">
-      <h1>SPAs!</h1>
-      <Animals />
-      <Plants />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>SPAs!</h1>
+
+        <ul>
+          <li>
+            <Link to="/animals">Animals</Link>
+          </li>
+          <li>
+            <Link to="/plants">Plants</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+
+
+        <Route path="/animals">
+          <Animals />
+        </Route>
+
+        <Route path="/plants">
+          <Plants />
+        </Route>
+
+        <Route path="/" exact> 
+          <Home />
+        </Route>
+
+      </div>
+    </Router>
   );
 }
 
